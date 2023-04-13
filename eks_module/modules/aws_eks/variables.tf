5f833c1b8d009a911cf685dac97be1050fde0e7c
vikash <vikash@gmail.com>
2023-04-13 21:20:51 +0530

    eks module updated

---
+++ eks_module/modules/aws_eks/variables.tf
@@ -0,0 +1,14 @@
+variable "eks_cluster_name" {
+     default  =  "demo-eks"
+}
+
+variable "subnet_ids" {
+     
+}
+variable "tags" {
+  
+}
+
+# variable "testsubnet_ids" {
+  
+# }
---
+++ eks_module/variables.tf
@@ -0,0 +1,42 @@
+
+variable "access_key" {
+    default = ""
+}
+variable "secret_key" {
+    default = ""
+}
+
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "aws_eks_cluster_config" {
+  
+}
+variable "eks_node_group_config" {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
